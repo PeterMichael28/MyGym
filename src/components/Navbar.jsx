4a8ab6c37418ceefdffa0bf864ed953a924ca752
peter michael <enitanpeters28@gmail.com>
2022-12-02 13:49:28 +0100

    pushed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,7 +8,7 @@ import Logo from '../assets/images/Logo.png'
 const Navbar = () => {
   return (
     <Stack direction='row' justifyContent='space-around' sx={{gap: {sm: '112px', xs: '40px'}, mt: { sm: '32px', xs: '20px'}, justifyContent: 'none'}} px='20px'>
-          <Link to='/'>
+          <Link to='/MyGym'>
               <img src={ Logo } alt="logo" style={ {width: '40px', height:'38px', margin: '0 20px'} } />
           </Link>
 
@@ -19,7 +19,7 @@ const Navbar = () => {
                 alignItems='flex-end'
             >
                 
-              <Link to='/' style={{textDecoration: 'none', color: '#3A1212', borderBottom: '3px solid #0bb1d6'}}>
+              <Link to='/MyGym' style={{textDecoration: 'none', color: '#3A1212', borderBottom: '3px solid #0bb1d6'}}>
                   Home
               </Link>
               <a href="#exercises" style={{textDecoration: 'none', color: '#3A1212'}}>Exercises</a>
